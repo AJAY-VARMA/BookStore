a755e9b842d1f9a380b5ace55b82f08810c76bb7
ajay <ajayvarma1101@gmail.com>
2020-07-07 10:08:15 +0530

    [ajay] added file to the mail operations

---
+++ book_store/resources/mail.py
@@ -0,0 +1,23 @@
+from flask_mail import Message,Mail
+import sys,os
+sys.path.append('.')
+from app import app
+
+mail = Mail(app)
+
+class MailService:
+    @staticmethod 
+    def send_mail(token,email,mail_user):
+        try:
+            msg = Message( 
+                    'Hello', 
+                    sender = mail_user, 
+                    recipients = [email]
+                ) 
+            msg.body = '''verification link : click the link for registration
+            http://127.0.0.1:5000/register-email/{}'''.format(token)
+            print(mail)
+            mail.send(msg)
+            return "check your mail click the link for verification"
+        except:
+           pass

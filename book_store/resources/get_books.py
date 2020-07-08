28b3f60b2540b2562e3b5340d91d139d8a37bbb2
ajayvarma <ajayvarma1101@gmail.com>
2020-07-08 23:23:22 +0530

    [ajay] refactor: code by returning respone instead of templates

--- book_store/resources/get_books.py
+++ book_store/resources/get_books.py
@@ -1,13 +1,25 @@
 from flask_restful import Resource
 from .db import DataBase
-from flask import make_response,render_template
+from flask import make_response,render_template,request,jsonify
+from .error_handler import InvalidUsageError
+import jinja2
 
 class GetBooks(Resource):
     def get(self):
-        try:
-            product_data = DataBase.get_data_from_db()
-            return make_response(render_template('get_books.html',product_data = product_data))
-        except:
-            pass
+        product_data = DataBase.get_data_from_db()
+        return make_response(jsonify({"books" : product_data}),200)
+        # try:
+        #     product_data = DataBase.get_data_from_db()
+        #     return make_response(render_template('get_books.html',product_data = product_data))
+        # except jinja2.exceptions.TemplateNotFound:
+        #     raise InvalidUsageError("template not found",404)
+
     def post(self):
-        pass
+        # try:
+            book_data = request.form
+            book_id = book_data['search']
+            product_data = DataBase.get_book_data_by_id(book_id)
+            return make_response(jsonify({"books" : product_data}),200)
+            # return make_response(render_template('get_books.html',product_data = product_data))
+        # except jinja2.exceptions.TemplateNotFound:
+        #     raise InvalidUsageError("template not found",404)

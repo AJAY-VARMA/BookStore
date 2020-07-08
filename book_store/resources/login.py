d6f3d94436240f79fbbda84dfb534e081ddb2f32
ajayvarma <ajayvarma1101@gmail.com>
2020-07-08 23:24:27 +0530

    [ajay] refactor: code by returning respone instead of templates and used validation

--- book_store/resources/login.py
+++ book_store/resources/login.py
@@ -1,22 +1,38 @@
 from flask_restful import Resource,Api
-from flask import make_response,render_template,request
+from flask import make_response,render_template,request,jsonify,redirect,url_for
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 import jinja2
 from .db import DataBase
+from .error_handler import InvalidUsageError
 
 class Login(Resource):
+
     def get(self):
-        try:
-            return make_response(render_template('login.html'))
-        except jinja2.exceptions.TemplateNotFound:
-            pass
+        return make_response(jsonify({"respone" : "get request called for login"}),200)
+        # try:
+        #     return make_response(render_template('login.html'))
+        # except jinja2.exceptions.TemplateNotFound:
+        #     raise InvalidUsageError("template not found",404)
 
     def post(self):
         # try:
-            user_details = request.form
-            user_name = user_details['name']
-            password = user_details['pswd']
-            return DataBase.check_user_in_db(user_name,password)
+            form = LoginForm(request.form)
+            user_name = form.username.data
+            password = form.password.data
+            if form.validate():
+                return Login.check_db(user_name,password)
+            return make_response(jsonify({"respone" : "validation failed"}),401)
         # except Exception:
         #     pass
 
+    @staticmethod
+    def check_db(user_name,password):
+        present_in_db =  DataBase.check_user_in_db(user_name,password)
+        if present_in_db:
+            return make_response(jsonify({"respone" : "login sucess"}),200)
+            # return redirect(url_for('getbooks'))
+        return make_response(jsonify({"respone" : "login failed"}),401)
 
+class LoginForm(Form):
+    username = StringField('username', [validators.Length(min=4, max=25)])
+    password = PasswordField('password', [validators.Length(min=8, max=50)])

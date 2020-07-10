966f3ffd0b3855cef548d43998a697cbcdbccd12
ajayvarma <ajayvarma1101@gmail.com>
2020-07-10 11:33:18 +0530

    [ajay] refactor: code by adding new routes

--- book_store/routes.py
+++ book_store/routes.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_restful import Api
-from resources import Login,Register,RegisterEmail,GetBooks
+from .resources import Login,Register,RegisterEmail,GetBooks,Sort,AddToCart
 import os
-
-from app import app
+from .app import app
 
 api = Api(app)
 
@@ -11,3 +10,5 @@ api.add_resource(Login,'/login')
 api.add_resource(Register,'/register')
 api.add_resource(RegisterEmail , '/register-email/<token>')
 api.add_resource(GetBooks,'/getbooks')
+api.add_resource(Sort,'/getbooks/sort-by-price')
+# api.add_resource(AddToCart,'/getbooks/add-to-cart')

57a5fbbf33785cde3b44f907f13b4377e545c667
ajayvarma <ajayvarma1101@gmail.com>
2020-07-10 11:31:17 +0530

    [ajay] refactor: by removing handled exceptions

--- book_store/resources/get_books.py
+++ book_store/resources/get_books.py
@@ -8,18 +8,11 @@ class GetBooks(Resource):
     def get(self):
         product_data = DataBase.get_data_from_db()
         return make_response(jsonify({"books" : product_data}),200)
-        # try:
-        #     product_data = DataBase.get_data_from_db()
-        #     return make_response(render_template('get_books.html',product_data = product_data))
-        # except jinja2.exceptions.TemplateNotFound:
-        #     raise InvalidUsageError("template not found",404)
 
     def post(self):
-        # try:
-            book_data = request.form
-            book_id = book_data['search']
-            product_data = DataBase.get_book_data_by_id(book_id)
-            return make_response(jsonify({"books" : product_data}),200)
-            # return make_response(render_template('get_books.html',product_data = product_data))
-        # except jinja2.exceptions.TemplateNotFound:
-        #     raise InvalidUsageError("template not found",404)
+            book_form = request.form
+            search_value = book_form['search']
+            product_data = DataBase.search_book(search_value)
+            if len(product_data) > 0:
+                return make_response(jsonify({"books" : product_data}),200)
+            return make_response(jsonify({"response":"the book u typed is not available"}),400)

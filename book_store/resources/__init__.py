c32cb581b62e9c47f52567d59a3979860cfcfe10
ajayvarma <ajayvarma1101@gmail.com>
2020-07-10 11:29:30 +0530

    [ajay] refactor: by adding some imports to init

--- book_store/resources/__init__.py
+++ book_store/resources/__init__.py
@@ -1,3 +1,7 @@
 from .login import Login
 from .register import Register,RegisterEmail
 from .get_books import GetBooks
+from .sort_by import Sort
+from .add_to_cart import AddToCart
+import sys,os
+sys.path.append('.')

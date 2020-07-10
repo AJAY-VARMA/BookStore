8bb08568e224c7de5c4a00a793931e43f2add8b9
ajayvarma <ajayvarma1101@gmail.com>
2020-07-10 11:27:49 +0530

    [ajay] refactor: by removing unwanted config

--- book_store/app.py
+++ book_store/app.py
@@ -1,7 +1,5 @@
 from flask import Flask,jsonify
 from dotenv import load_dotenv
-from resources.error_handler import InvalidUsageError
-# from .resources.error_handler import InvalidUsageError
 import os
 app = Flask(__name__)
 
@@ -12,25 +10,13 @@ app.config['MAIL_PORT'] = os.getenv("mail_port")
 app.config['MAIL_USERNAME'] = os.getenv("mail_user")
 app.config['MAIL_PASSWORD'] = os.getenv("mail_pswd")
 app.config['MAIL_USE_SSL'] = True
+app.config['secret_key'] = os.getenv("secret_key")
 
-# app.config['MYSQL_HOST'] = os.getenv("sql_host")
-# app.config['MYSQL_USER'] = os.getenv("sql_user")
-# app.config['MYSQL_PASSWORD'] = os.getenv("sql_pswd")
-# app.config['MYSQL_DB'] = os.getenv("sql_db")
-# app.config['secret_key'] = os.getenv("secret_key")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("connection")
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# c = os.getenv("sql_host")
-# print(c)
-
-
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "1234"
-app.config['MYSQL_DB'] = "bookstore"
-app.config['secret_key'] = "secretkey"
-
-@app.errorhandler(InvalidUsageError)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+# @app.errorhandler(InvalidUsageError)
+# def handle_invalid_usage(error):
+#     response = jsonify(error.to_dict())
+#     response.status_code = error.status_code
+#     return response

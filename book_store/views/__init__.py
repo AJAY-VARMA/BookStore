d593f4272aaa69d4d68e0336c49f2458abd04ebd
ajayvarma <ajayvarma1101@gmail.com>
2020-07-15 17:58:01 +0530

    [ajay] added: file to make package

---
+++ book_store/views/__init__.py
@@ -0,0 +1,7 @@
+from .auth import Login,Register,RegisterEmail
+from .get_books import GetBooks,Sort
+# from .sort_by import Sort
+from .cart_and_wishlist import Cart,Wishlist
+# from .wishlist import Wishlist
+import sys,os
+sys.path.append('.')

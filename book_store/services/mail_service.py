c0b7ef4df988e930926a6adc426fefdf665ce3be
ajayvarma <ajayvarma1101@gmail.com>
2020-07-15 17:55:08 +0530

    [ajay] added: file for the mail services

---
+++ book_store/services/mail_service.py
@@ -0,0 +1,30 @@
+from flask_mail import Message,Mail
+from app import app
+# from book_store.app  import app
+from .error_handler_service import InvalidUsageError
+import os,jwt
+from dotenv import load_dotenv
+
+load_dotenv('bookenv/.env')
+secret_key = os.getenv('secret_key')
+mail_user = os.getenv('mail_user')
+
+mail = Mail(app)
+
+class MailService:
+    @staticmethod 
+    def send_mail(user_name,email):
+        try:
+            token = jwt.encode({'email':email}
+                    ,secret_key).decode('utf-8')
+            msg = Message( 
+                    'Hello', 
+                    sender = mail_user,
+                    recipients = [email]
+                ) 
+            msg.body = '''verification link : click the link for registration
+            http://127.0.0.1:5000/register-email/{}'''.format(token)
+            mail.send(msg)
+            return "check your mail click the link for verification"
+        except (NameError,AssertionError):
+           raise InvalidUsageError('error in mail service', 500)

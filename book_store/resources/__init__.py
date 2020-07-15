8e53a0dbb5bfde2e4882ee1d3cd2125cefc02b2a
ajayvarma <ajayvarma1101@gmail.com>
2020-07-15 17:38:22 +0530

    [ajay] deleted resources package and named as vies

--- book_store/resources/__init__.py
+++
@@ -1,7 +0,0 @@
-from .login import Login
-from .register import Register,RegisterEmail
-from .get_books import GetBooks
-from .sort_by import Sort
-from .add_to_cart import AddToCart
-import sys,os
-sys.path.append('.')

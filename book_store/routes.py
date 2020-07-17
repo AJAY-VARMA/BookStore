766688c1644616eee9f40d93ff4993ba9e4bb3a7
ajayvarma <ajayvarma1101@gmail.com>
2020-07-17 22:32:49 +0530

    [ajay] refactor: code by adding new routes

--- book_store/routes.py
+++ book_store/routes.py
@@ -1,15 +1,11 @@
 from flask import Flask
 from flask_restful import Api
-from views import Login,Register,RegisterEmail,GetBooks,Sort,Cart,Wishlist
-import os
+from views import Login,Register,RegisterEmail,GetBooks,Sort,Cart,Wishlist,Order,CheckOut
 from app import app
-from flask_login import LoginManager
 from flask_jwt_extended import  JWTManager
 
 api = Api(app)
 jwt = JWTManager(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 api.add_resource(Login,'/login')
 api.add_resource(Register,'/register')
@@ -18,3 +14,5 @@ api.add_resource(GetBooks,'/getbooks')
 api.add_resource(Sort,'/getbooks/sort-by-price')
 api.add_resource(Cart,'/getbooks/add-to-cart')
 api.add_resource(Wishlist,'/getbooks/add-to-wishlist')
+api.add_resource(Order,'/getbooks/add-to-cart/order')
+api.add_resource(CheckOut,'/getbooks/add-to-cart/checkout')

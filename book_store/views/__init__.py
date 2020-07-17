ac58d28ba0fb86e8ab54032c21b62ea87454f74f
ajayvarma <ajayvarma1101@gmail.com>
2020-07-17 22:35:43 +0530

    [ajay] refactor: code by adding new classes

--- book_store/views/__init__.py
+++ book_store/views/__init__.py
@@ -1,6 +1,6 @@
 from .auth import Login,Register,RegisterEmail
 from .get_books import GetBooks,Sort
-# from .sort_by import Sort
+from .order import Order,CheckOut
 from .cart_and_wishlist import Cart,Wishlist
 # from .wishlist import Wishlist
 import sys,os

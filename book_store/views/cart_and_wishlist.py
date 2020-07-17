190cf4194363b5c43669f86d68a4eb0f0844ea8e
ajayvarma <ajayvarma1101@gmail.com>
2020-07-17 22:37:06 +0530

    [ajay] refactor: code by functionality for the put method for cart

--- book_store/views/cart_and_wishlist.py
+++ book_store/views/cart_and_wishlist.py
@@ -2,14 +2,13 @@ from flask_restful import Resource
 from flask import request,jsonify,make_response
 from services.db_services import DataBase
 from flask_jwt_extended import jwt_required,get_jwt_identity
+from services.response import *
 
 class Cart(Resource):
     @jwt_required
     def get(self):
         user_name = get_jwt_identity()
-        price = 0
-        books_in_cart,price = DataBase.display_cart(user_name)
-        return price
+        books_in_cart = DataBase.display_cart(user_name)
         return make_response(jsonify({"respone" : books_in_cart}),200)
 
     @jwt_required
@@ -18,7 +17,16 @@ class Cart(Resource):
         user_name = get_jwt_identity()
         product_id = data['productid']
         DataBase.add_to_cart(user_name,product_id)
-        return make_response(jsonify({"respone" : "added to cart"}),200)
+        return make_response(cart[200],200)
+
+    @jwt_required
+    def put(self):
+        data =  request.form
+        user_name = get_jwt_identity()
+        product_id = int(data['productid'])
+        quantity = int(data['quantity'])
+        books_in_cart = DataBase.update_cart(user_name,product_id,quantity)
+        return make_response(jsonify({"respone" : books_in_cart}),200)
 
 class Wishlist(Resource):
     @jwt_required
@@ -33,4 +41,4 @@ class Wishlist(Resource):
         user_name = get_jwt_identity()
         product_id = data['productid']
         DataBase.add_to_wishlist(user_name,product_id)
-        return make_response(jsonify({"respone" : "added to wishlist"}),200)
+        return make_response(wishlist[200],200)

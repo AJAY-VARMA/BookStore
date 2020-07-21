ccda34163a435fb4554111f93117d591108ddfeb
ajayvarma <ajayvarma1101@gmail.com>
2020-07-21 10:18:23 +0530

    [ajay] refactor: code by adding config of sqlalchemy

--- book_store/app.py
+++ book_store/app.py
@@ -1,9 +1,9 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,send_from_directory
 from dotenv import load_dotenv
 import os
 from services.error_handler_service import InvalidUsageError
-app = Flask(__name__)
 
+app = Flask(__name__)
 load_dotenv('bookenv/.env')
  
 app.config['MAIL_SERVER'] = os.getenv("mail_server")
@@ -12,9 +12,9 @@ app.config['MAIL_USERNAME'] = os.getenv("mail_user")
 app.config['MAIL_PASSWORD'] = os.getenv("mail_pswd")
 app.config['MAIL_USE_SSL'] = True
 app.config['secret_key'] = os.getenv("secret_key")
-app.config['JWT_SECRET_KEY'] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
+app.config['JWT_SECRET_KEY'] = os.getenv("jwt_secret_key")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("connection")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.errorhandler(InvalidUsageError)
 def handle_invalid_usage(error):

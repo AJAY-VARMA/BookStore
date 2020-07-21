4975ff1aaf251f2dc899a95d17b9c647b26ef441
ajayvarma <ajayvarma1101@gmail.com>
2020-07-21 10:22:28 +0530

    [ajay] refactor: code by adding checkout class

--- book_store/views/__init__.py
+++ book_store/views/__init__.py
@@ -2,6 +2,5 @@ from .auth import Login,Register,RegisterEmail
 from .get_books import GetBooks,Sort
 from .order import Order,CheckOut
 from .cart_and_wishlist import Cart,Wishlist
-# from .wishlist import Wishlist
 import sys,os
 sys.path.append('.')

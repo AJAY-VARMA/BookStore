e2cc09b2c3986be3a5980cad03d2b45dfb8360d6
ajayvarma <ajayvarma1101@gmail.com>
2020-07-21 15:17:49 +0530

    [ajay] refactor: code by initialize app for db and mail

--- book_store/app.py
+++ book_store/app.py
@@ -1,11 +1,21 @@
-from flask import Flask,jsonify,send_from_directory
+from flask import Flask,jsonify
 from dotenv import load_dotenv
 import os
 from services.error_handler_service import InvalidUsageError
+from model.model import db
+from services.mail_service import mail
+from routes import initialize_routes
+from flask_jwt_extended import  JWTManager
+from flask_restful_swagger import swagger
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
+initialize_routes(api)
+jwt = JWTManager(app)
+api = swagger.docs(Api(app),apiVersion='3.0',api_spec_url='/docs')
 load_dotenv('bookenv/.env')
- 
+
 app.config['MAIL_SERVER'] = os.getenv("mail_server")
 app.config['MAIL_PORT'] = os.getenv("mail_port")
 app.config['MAIL_USERNAME'] = os.getenv("mail_user")
@@ -15,6 +25,9 @@ app.config['secret_key'] = os.getenv("secret_key")
 app.config['JWT_SECRET_KEY'] = os.getenv("jwt_secret_key")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("connection")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+mail.init_app(app)
+db.init_app(app)
 
 @app.errorhandler(InvalidUsageError)
 def handle_invalid_usage(error):

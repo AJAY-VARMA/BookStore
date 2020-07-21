aa4017ecb469ddfa87b0a09bb4bde3efa9bb2ab4
ajayvarma <ajayvarma1101@gmail.com>
2020-07-21 15:20:52 +0530

    [ajay] refactor: code by adding routes in function

--- book_store/routes.py
+++ book_store/routes.py
@@ -1,21 +1,13 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask,Blueprint
 from views import Login,Register,RegisterEmail,GetBooks,Sort,Cart,Wishlist,Order,CheckOut
-from app import app
-from flask_jwt_extended import  JWTManager
-from flask_restful_swagger import swagger
 
-api = Api(app)
-jwt = JWTManager(app)
-
-api = swagger.docs(Api(app),apiVersion='3.0',api_spec_url='/docs')
-
-api.add_resource(Login,'/login')
-api.add_resource(Register,'/register')
-api.add_resource(RegisterEmail , '/register-email/<token>')
-api.add_resource(GetBooks,'/getbooks')
-api.add_resource(Sort,'/getbooks/sort-by-price')
-api.add_resource(Cart,'/getbooks/add-to-cart')
-api.add_resource(Wishlist,'/getbooks/add-to-wishlist')
-api.add_resource(Order,'/getbooks/add-to-cart/order')
-api.add_resource(CheckOut,'/getbooks/add-to-cart/checkout')
+def initialize_routes(api):
+    api.add_resource(Login,'/login')
+    api.add_resource(Register,'/register')
+    api.add_resource(RegisterEmail , '/register-email/<token>')
+    api.add_resource(GetBooks,'/getbooks')
+    api.add_resource(Sort,'/getbooks/sort-by-price')
+    api.add_resource(Cart,'/getbooks/add-to-cart')
+    api.add_resource(Wishlist,'/getbooks/add-to-wishlist')
+    api.add_resource(Order,'/getbooks/add-to-cart/order')
+    api.add_resource(CheckOut,'/getbooks/add-to-cart/checkout')

5f4fa21f890b36c6d595fd5fc6de5e48a162f082
ajayvarma <ajayvarma1101@gmail.com>
2020-07-22 14:49:02 +0530

    [ajay] refactor: code by adding config for redis and initialize the reddis

--- book_store/app.py
+++ book_store/app.py
@@ -4,16 +4,18 @@ import os
 from services.error_handler_service import InvalidUsageError
 from model.model import db
 from services.mail_service import mail
+from services.db_services import redis
 from routes import initialize_routes
 from flask_jwt_extended import  JWTManager
 from flask_restful_swagger import swagger
 from flask_restful import Api
+from flask_redis import FlaskRedis
+
+
 
 app = Flask(__name__)
 api = Api(app)
-initialize_routes(api)
 jwt = JWTManager(app)
-api = swagger.docs(Api(app),apiVersion='3.0',api_spec_url='/docs')
 load_dotenv('bookenv/.env')
 
 app.config['MAIL_SERVER'] = os.getenv("mail_server")
@@ -25,9 +27,14 @@ app.config['secret_key'] = os.getenv("secret_key")
 app.config['JWT_SECRET_KEY'] = os.getenv("jwt_secret_key")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("connection")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['REDIS_URL'] = "redis://localhost:6379/0"
+
+redis.init_app(app)
 
 mail.init_app(app)
 db.init_app(app)
+api = swagger.docs(Api(app),apiVersion='3.0',api_spec_url='/docs')
+initialize_routes(api)
 
 @app.errorhandler(InvalidUsageError)
 def handle_invalid_usage(error):

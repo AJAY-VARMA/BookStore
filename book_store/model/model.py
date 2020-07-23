61122fc27a9bcd6c58a51a6dad5c700a32fcda85
ajayvarma <ajayvarma1101@gmail.com>
2020-07-23 12:11:26 +0530

    [ajay] refactor: code by changing the model names

--- book_store/model/model.py
+++ book_store/model/model.py
@@ -12,14 +12,13 @@ cart_relation = db.Table('cart_relation',
         db.Column('product_id',db.Integer,db.ForeignKey('product_data.pid'))
         )
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
     confirmed = db.Column(db.Boolean, nullable=False)
-    address = db.relationship('OrderDetails',backref = 'user',uselist = False)
+    address = db.relationship('OrderDetails',backref = 'user')
 
 class ProductData(db.Model):
     pid = db.Column(db.Integer, primary_key=True)

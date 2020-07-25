e1ecf94d393f4cc3f6738fc253991f7d434b55fc
ajayvarma <ajayvarma1101@gmail.com>
2020-07-25 14:28:59 +0530

    [ajay] refactor: code by adding new swagger parameters

--- book_store/swag.py
+++ book_store/swag.py
@@ -13,8 +13,7 @@ login = [
               "type": "string",
               "paramType": "form"
             }
- ]
-
+]
 register = [
             {
               "name": "username",
@@ -51,22 +50,31 @@ search = [
               "description": "enter author or book name to search",
               "required": True,
               "type": "string",
-              "paramType": "param"
+              "paramType": "query"
             }
           ]
+token = [
+        {
+              "name": "token",
+              "description": "enter token to register",
+              "required": True,
+              "type": "string",
+              "paramType": "path"
+            }
+]
 sort = [
             {
               "name": "sort",
               "description": "enter sort value according to price",
               "required": True,
               "type": "string",
-              "paramType": "param"
+              "paramType": "query"
             }
       ]
 jwt = [
         {
           "name": "Authorization",
-          "description": "jwt token for the wishlist",
+          "description": "jwt token for the Authorization",
           "required": True,
           "type": "string",
           "paramType": "header"
@@ -75,13 +83,13 @@ jwt = [
 product = [
             {
               "name": "Authorization",
-              "description": "jwt token for the wishlist",
+              "description": "jwt token for the Authorization",
               "required": True,
               "type": "string",
               "paramType": "header"
             },{
               "name": "productid",
-              "description": "product id to add to wishlist",
+              "description": "product id to add ",
               "required": True,
               "type": "string",
               "paramType": "form"
@@ -97,3 +105,35 @@ quantity = [
             }
 ]
 quantity = product + quantity
+
+order = [
+        {
+              "name": "name",
+              "description": "name of the customer",
+              "required": True,
+              "type": "sting",
+              "paramType": "form"
+            },
+            {
+              "name": "mobilenumber",
+              "description": "mobilenumber of the customer",
+              "required": True,
+              "type": "string",
+              "paramType": "form"
+            },
+            {
+              "name": "address",
+              "description": "address for the order",
+              "required": True,
+              "type": "string",
+              "paramType": "form"
+            },
+            {
+              "name": "pincode",
+              "description": "pincode for the order",
+              "required": True,
+              "type": "string",
+              "paramType": "form"
+            }
+]
+order  = jwt + order

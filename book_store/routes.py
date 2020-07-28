383d41c8eabb5fa69b7c80c24b90b23e23314838
ajayvarma <ajayvarma1101@gmail.com>
2020-07-28 13:35:32 +0530

    [ajay] refactor: code by changing the class names

--- book_store/routes.py
+++ book_store/routes.py
@@ -1,13 +1,13 @@
 from flask import Flask,Blueprint
-from views import Login,Register,RegisterEmail,GetBooks,Sort,Cart,Wishlist,Order,CheckOut
+from views import Login,Register,RegisterEmail,Books,Sort,Cart,Wishlist,Order,CheckOut
 
 def initialize_routes(api):
     api.add_resource(Login,'/login')
     api.add_resource(Register,'/register')
-    api.add_resource(RegisterEmail , '/register-email/<token>')
-    api.add_resource(GetBooks,'/getbooks')
-    api.add_resource(Sort,'/getbooks/sort-by-price')
-    api.add_resource(Cart,'/getbooks/add-to-cart')
-    api.add_resource(Wishlist,'/getbooks/add-to-wishlist')
-    api.add_resource(Order,'/getbooks/add-to-cart/order')
-    api.add_resource(CheckOut,'/getbooks/add-to-cart/checkout')
+    api.add_resource(RegisterEmail , '/register/<token>')
+    api.add_resource(Books,'/books')
+    api.add_resource(Sort,'/books/sort')
+    api.add_resource(Cart,'/books/cart')
+    api.add_resource(Wishlist,'/books/wishlist')
+    api.add_resource(Order,'/books/cart/order')
+    api.add_resource(CheckOut,'/books/cart/checkout')

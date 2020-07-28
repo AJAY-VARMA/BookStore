e085767ef814aa955a51e11748a76af89f682d1e
ajayvarma <ajayvarma1101@gmail.com>
2020-07-28 13:40:12 +0530

    [ajay] refactor: code by adding redis functionality to check login or not

--- book_store/views/cart_and_wishlist.py
+++ book_store/views/cart_and_wishlist.py
@@ -5,23 +5,33 @@ from flask_jwt_extended import jwt_required,get_jwt_identity
 from services.response import *
 from flask_restful_swagger import swagger
 from swag import jwt,product,quantity
+from redis.exceptions import ConnectionError
+# from decorator import check_token
+from flask_redis import FlaskRedis
+# from app import app
+redis = FlaskRedis()
 
 class Cart(Resource):
     @swagger.operation(notes = 'get cart',parameters = jwt)
     @jwt_required
     def get(self):
-        user_name = get_jwt_identity()
-        books_in_cart = DataBase.display_cart(user_name)
-        return make_response(jsonify({"respone" : books_in_cart}),200)
-
+        try:
+            user_name = get_jwt_identity()
+            if redis.exists(user_name):
+                books_in_cart = DataBase.display_cart(user_name)
+                return make_response(jsonify({"respone" : books_in_cart,"status" : 200}),200)
+            return make_response(login_response[413],413)
+        except ConnectionError :
+            raise InvalidUsageError(redis_error[500],500)
+            
+     
     @swagger.operation(notes = 'post cart',parameters = product)
     @jwt_required
     def post(self):
         data =  request.form
         user_name = get_jwt_identity()
         product_id = data['productid']
-        msg = DataBase.add_to_cart(user_name,product_id)
-        return make_response(msg,200)
+        return DataBase.add_to_cart(user_name,product_id)
 
     @swagger.operation(notes = 'delete cart',parameters = product)
     @jwt_required
@@ -30,15 +40,15 @@ class Cart(Resource):
         user_name = get_jwt_identity()
         product_id = data['productid']
         DataBase.remove_from_cart(user_name,product_id)
-        return make_response(cart_delete[200],200)
+        return make_response(response['deleted'],200)
 
     @swagger.operation(notes = 'put cart',parameters = quantity)
     @jwt_required
     def put(self):
         data =  request.form
         user_name = get_jwt_identity()
-        product_id = int(data['productid'])
-        quantity = int(data['quantity'])
+        product_id = data['productid']
+        quantity = data['quantity']
         books_in_cart,status_code = DataBase.update_cart(user_name,product_id,quantity)
         return make_response(jsonify({"respone" : books_in_cart}),status_code)
 
@@ -48,7 +58,7 @@ class Wishlist(Resource):
     def get(self):
         user_name = get_jwt_identity()
         books_data = DataBase.display_wishlist(user_name)
-        return make_response(jsonify({"respone" : books_data}),200)
+        return make_response(jsonify({"respone" : books_data,"status" : 200}),200)
 
     @swagger.operation(notes = 'post wishlist',parameters = product)
     @jwt_required
@@ -56,5 +66,5 @@ class Wishlist(Resource):
         data = request.form
         user_name = get_jwt_identity()
         product_id = data['productid']
-        msg = DataBase.add_to_wishlist(user_name,product_id)
-        return make_response(msg,200)
+        response = DataBase.add_to_wishlist(user_name,product_id)
+        return make_response(response,200)

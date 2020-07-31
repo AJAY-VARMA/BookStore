3c2978805900912562285127938873a674dd1582
ajayvarma <ajayvarma1101@gmail.com>
2020-07-31 14:11:33 +0530

    [ajay] refactor: code by changing the class name of the imported names

--- book_store/views/__init__.py
+++ book_store/views/__init__.py
@@ -1,4 +1,6 @@
 from .auth import Login,Register,RegisterEmail
-from .get_books import GetBooks,Sort
+from .books import Books,Sort
 from .order import Order,CheckOut
 from .cart_and_wishlist import Cart,Wishlist
+import sys,os
+sys.path.append('.')

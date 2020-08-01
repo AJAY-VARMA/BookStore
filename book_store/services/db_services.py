5dc95a9e40c45ae81df2f05e81ef9996a531040c
ajayvarma <ajayvarma1101@gmail.com>
2020-08-01 11:14:27 +0530

    [ajay] refactor: code by adding get order id method

--- book_store/services/db_services.py
+++ book_store/services/db_services.py
@@ -10,9 +10,10 @@ import os,jwt,random,json,time
 from dotenv import load_dotenv
 from .mail_service import MailService
 from .response import *
-from .data import order_max,order_min
+from .static_data import order_max,order_min,redis_time
 from flask_redis import FlaskRedis
 from redis.exceptions import ConnectionError
+from .serializer import ProductSchema,ProductOrderSchema
 
 redis = FlaskRedis()
 
@@ -21,15 +22,20 @@ secret_key = os.getenv('secret_key')
 
 class DataBase:
     @staticmethod
-    def change_products_quantity_in_db():
+    def change_products_quantity_in_db(username):
         try:
             books_data = json.loads(redis.get('books_data'))
+            user = User.query.filter_by(username = username).first()
+            order = OrderData.query.filter_by(user_id = user.id).first()
             list_of_books = books_data[:-1]
             for each_book in list_of_books:
                 quantity = each_book['quantity']
-                book_id = each_book['book_id']
+                book_id = each_book['pid']
                 book_data = ProductData.query.filter_by(pid = book_id).first()
                 book_data.quantity = book_data.quantity - quantity
+                db.session.commit()
+                order_with_products  = OrderWithProducts(order_id = order.orderid,product_id = book_id,quantity = quantity)
+                db.session.add(order_with_products)
                 db.session.commit()
                 redis.delete('books')
         except (InvalidRequestError,OperationalError,CompileError):
@@ -38,14 +44,22 @@ class DataBase:
             raise InvalidUsageError(redis_error[500],500)
 
     @staticmethod
+    def get_order_id():
+        order_id = random.randint(order_min,order_max)
+        present = bool(OrderData.query.filter_by(orderid = order_id).first())
+        if present:
+            DataBase.get_order_id()
+        return order_id
+
+    @staticmethod
     def add_order(username):
         try:
             books_data = json.loads(redis.get('books_data'))
             list_of_books = books_data[-1]
             total_price = list_of_books['total-Price']
             user = User.query.filter_by(username = username).first()
-            order_id = random.randint(order_min,order_max)
             address = UserAddress.query.filter_by(user_id = user.id).first()
+            order_id = DataBase.get_order_id()
             order = OrderData(user_id= user.id,address_id = address.address_id,orderid = order_id,total_price = total_price)
             db.session.add(order)
             db.session.commit()
@@ -62,7 +76,8 @@ class DataBase:
             address_data = UserAddress.query.filter_by(user_id = user.id).first()
             order = OrderData.query.filter_by(user_id = user.id).first()
             list_of_books = books_data[:-1]
-            books =  DataBase.add_keys(list_of_books)
+            products_schema =  ProductOrderSchema(many = True)
+            books = products_schema.dump(list_of_books)
             price = DataBase.calculate_price(books)
             books.append({"total-Price" : price})
             user_mail = user.email
@@ -73,21 +88,6 @@ class DataBase:
             return list_of_details,books,details_to_send_msg
         except (InvalidRequestError,OperationalError):
             raise InvalidUsageError(sql[500], 500)
-
-    @staticmethod
-    def add_keys(books_data):
-        book_list = []
-        for each_book in books_data:
-            book_list.append(
-                {
-                "author" : each_book["author"],
-                "title" : each_book["title"],
-                "price" : each_book["price"],
-                "quantity" : each_book["quantity"],
-                "image" : each_book["image"]
-                }
-            )
-        return book_list
 
     @staticmethod
     def add_to_address_db(username,name,mobile_number,address,pincode):
@@ -103,17 +103,16 @@ class DataBase:
 
     @staticmethod
     def display_wishlist(user_name):
-        list_of_books = []
         try:
             if redis.exists("items"):
                 return json.loads(redis.get('items'))
             user = User.query.filter_by(username = user_name).first()
-            for each_book in user.wishlist:
-                list_of_books.append(each_book)
-            books_with_keys = DataBase.to_add_keys(list_of_books)
-            json_books = json.dumps(books_with_keys)
+            list_of_books = [each_book for each_book in user.wishlist]
+            products_schema =  ProductSchema(many = True)
+            books = products_schema.dump(list_of_books)
+            json_books = json.dumps(books)
             redis.set(name = 'items',value = json_books)
-            redis.expire('items' ,(2*60*60))
+            redis.expire('items' ,redis_time)
             return books_with_keys
         except (InvalidRequestError,OperationalError):
             raise InvalidUsageError(sql[500], 500)
@@ -148,9 +147,8 @@ class DataBase:
     
     @staticmethod
     def calculate_price(books):
-        price = 0 
-        for each_book in books:
-            price += each_book['price']
+        price = 0
+        [price := price + each_book['price'] for each_book in books]
         return price
     
     @staticmethod
@@ -183,21 +181,20 @@ class DataBase:
             product_id = int(product_id)
             book_data = ProductData.query.filter_by(pid = product_id).first()
             if book_data.quantity < quantity:
-                return cart_quantity[400],400
+                return response['quantity'],400
             books_data = json.loads(redis.get('books_data'))
             list_of_books = books_data[:-1]
             for each_book in list_of_books:
-                if each_book['book_id'] == product_id:
+                if each_book['pid'] == product_id:
                     is_product_present = True
                     each_book["quantity"] = quantity
                     price = book_data.price
-                    # price = each_book["price"]
                     each_book["price"] = price * quantity
             price = DataBase.calculate_price(list_of_books)
             list_of_books.append({"total-Price" : price})
             json_books = json.dumps(list_of_books)
             redis.set(name = 'books_data',value = json_books)
-            redis.expire('books_data' ,(24*60*60))
+            redis.expire('books_data' ,redis_time)
             if is_product_present:
                 return list_of_books,200
             return response[400],400
@@ -214,15 +211,16 @@ class DataBase:
             if redis.exists('books_data'):
                 return json.loads(redis.get('books_data'))
             user = User.query.filter_by(username = user_name).first()
-            list_of_books = DataBase.get_list_of_books(user)
-            books =  DataBase.to_add_keys(list_of_books)
+            list_of_books = [each_book for each_book in user.cart]
+            products_schema =  ProductSchema(many = True)
+            books = products_schema.dump(list_of_books) 
             for each_book in books:
                 each_book["quantity"] = 1
             price = DataBase.calculate_price(books)
             books.append({"total-Price" : price})
             json_books = json.dumps(books)
             redis.set(name = 'books_data',value = json_books)
-            redis.expire('books_data' ,(24*60*60))
+            redis.expire('books_data' ,redis_time)
             return books
         except (InvalidRequestError,OperationalError,CompileError):
             raise InvalidUsageError(sql[500], 500)
@@ -257,35 +255,17 @@ class DataBase:
         except (InvalidRequestError,OperationalError,CompileError) :
             raise InvalidUsageError(sql[500], 500)
 
-
     # static methods  for search,and get books
     # for get book api  
     @staticmethod
     def search_book(search_value):
         try:
-            books_data = ProductData.query.filter_by(author = search_value).all()
-            if len(books_data) == 0:
-                books_data = ProductData.query.filter_by(title = search_value).all()
-            return DataBase.to_add_keys(books_data)
+            books_data = ProductData.query.filter((ProductData.author == search_value) | (ProductData.title == search_value)).all()
+            products_schema =  ProductSchema(many = True)
+            all_books = products_schema.dump(books_data) 
+            return all_books
         except (InvalidRequestError,OperationalError) :
             raise InvalidUsageError(sql[500], 500)
-
-    @staticmethod
-    def to_add_keys(books_data):
-        book_list = []
-        for each_book in books_data:
-            book_list.append(
-                {
-                "book_id":each_book.pid,
-                "author" : each_book.author,
-                "title" : each_book.title,
-                "image" : each_book.image,
-                "quantity" : each_book.quantity,
-                "price" : each_book.price,
-                "description":each_book.description
-                }
-            )
-        return book_list
 
     @staticmethod
     def get_books_data_from_db():
@@ -293,16 +273,16 @@ class DataBase:
             if redis.exists("books"):
                 return json.loads(redis.get('books'))
             books_data = ProductData.query.all()
-            books_with_keys =  DataBase.to_add_keys(books_data)
-            json_books = json.dumps(books_with_keys)
+            products_schema =  ProductSchema(many = True)
+            all_books = products_schema.dump(books_data)
+            json_books = json.dumps(all_books) 
             redis.set(name = 'books',value = json_books)
-            redis.expire('books' ,(2*60*60))
-            return books_with_keys
+            redis.expire('books' ,redis_time)
+            return all_books
         except (InvalidRequestError,OperationalError):
             raise InvalidUsageError(sql[500], 500)
         except (ConnectionError,TypeError):
             raise InvalidUsageError(redis_error[500],500)
-
 
     # static method to add user in db
     # for registration api
@@ -319,7 +299,6 @@ class DataBase:
             return make_response(registration_response[409],409)
         except (InvalidRequestError,OperationalError):
             raise InvalidUsageError(sql[500], 500)
-
 
     # static method to check user 
     # for login api

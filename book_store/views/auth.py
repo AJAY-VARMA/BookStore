a7db1898f39d67ccf13b12660250250a7d0a2501
ajayvarma <ajayvarma1101@gmail.com>
2020-08-01 11:16:51 +0530

    [ajay] refactor: by calling reddis from DataBase class

--- book_store/views/auth.py
+++ book_store/views/auth.py
@@ -1,21 +1,16 @@
 from flask_restful import Resource
 from flask import make_response,request,jsonify
-from services.db_services import DataBase
+from services.db_services import DataBase,redis
 from services.response import *
 from flask_jwt_extended import create_access_token
 from form import RegisterForm,LoginForm
+from flask import current_app as app
 from flask_restful_swagger import swagger
 from swag import login,register,token
-from flask import current_app as app
 from flask_redis import FlaskRedis
 from redis.exceptions import ConnectionError
 from services.error_handler_service import InvalidUsageError
-
-reddis = FlaskRedis()
-
-# def check_token(func):
-#     redi   redi.set(name = "xyx",value = "xyz")
-#     return func
+from services.static_data import redis_time
 
 class Login(Resource):
 
@@ -32,12 +27,11 @@ class Login(Resource):
                 if not confirmed:
                     return make_response(login_response[411],411)
                 access_token = create_access_token(identity=user_name)
-                # app.logger.info('{} was logged in'.format(user_name))
-                reddis.set(name = user_name,value = access_token)
-                reddis.expire(user_name ,(23*60*60))
+                app.logger.info('{} was logged in'.format(user_name))
+                redis.set(name = user_name,value = access_token)
+                redis.expire(user_name ,redis_time)
                 return make_response(jsonify({"respone:token" : access_token,"status": 200}),200)
-            return make_response(login_response[401],401)
-            # app.logger.error(form.username.errors+form.password.errors)        
+            return make_response(login_response[401],401)        
         except ConnectionError:
             raise InvalidUsageError(redis_error[500],500)
 
